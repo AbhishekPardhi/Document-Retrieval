dfec12226a4e14f418b5aaba10f37ea6fc5f0e64
Abhishek Pardhi <apardhi20@iitk.ac.in>
2023-11-20 19:38:32 +0530

    Memory working but not in App

--- frontend/app.py
+++ frontend/app.py
@@ -12,7 +12,11 @@ from dotenv import load_dotenv
 import qdrant_client
 from langchain.vectorstores import Qdrant
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.llms import OpenAI
+# from langchain.llms import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.memory import ConversationSummaryMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQAWithSourcesChain
 
 load_dotenv('../.env')
@@ -22,11 +26,49 @@ QDRANT_API_KEY = os.getenv('QDRANT_API_KEY')
 COLLECTION_NAME = os.getenv('COLLECTION_NAME')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
-K=4
+K=2
 
-# neural_searcher = NeuralSearcher(collection_name=COLLECTION_NAME)
+#############################################################################################################
+#############################################################################################################
 
-@st.cache_resource
+@st.cache_resource(show_spinner=False)
+def PROMPT():
+    prompt_template = '''
+    You are a Product Recommendation Agent who gets his context from the retrieved descriptions of the products that matches best with the User's query.
+    Answer my questions based on your knowledge and our older conversation. Do not make up answers.
+    If you do not know the answer to a question, just say "I don't know" in a polite manner.
+
+    {context}
+
+    Given the following conversation and a follow up question, answer the question.
+
+    {chat_history}
+
+    question: {question}
+    '''
+
+    return PromptTemplate(
+        template=prompt_template, input_variables=["context", "chat_history", "question"]
+    )
+
+@st.cache_resource(show_spinner=False)
+def load_model():
+    return ChatOpenAI(model_name='gpt-3.5-turbo')
+
+llm = load_model()
+
+def memory():
+    if 'memory' not in st.session_state:
+        st.session_state.memory = ConversationSummaryMemory(
+            llm=llm,
+            memory_key="chat_history",
+            return_messages=True,
+            output_key='answer'
+        )
+    return st.session_state.memory
+
+
+# @st.cache_resource(show_spinner=False)
 def Retriever():
     global K
     client = qdrant_client.QdrantClient(
@@ -42,22 +84,65 @@ def Retriever():
         embeddings=embeddings,
     )
 
-    llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+    retriever = vector_store.as_retriever(search_kwargs={'k':K})
 
-    qa = RetrievalQAWithSourcesChain.from_chain_type(
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
         llm=llm,
         chain_type="stuff",
-        retriever=vector_store.as_retriever(search_kwargs={'k':K}),
+        retriever=retriever,
+        memory=memory(),
         return_source_documents=True,
         reduce_k_below_max_tokens=True
     )
 
-    return qa
+    return chain
 
-@st.cache_data
+@st.cache_data(show_spinner=False)
 def search(_retriever, user_question):
-    res = _retriever({"question": user_question})
+    # res = _retriever({"question": user_question})
+    res = _retriever(PROMPT().format(question=user_question, chat_history=memory().chat_memory.messages, context=''))
+    print(memory().load_memory_variables({})['chat_history'])
     return res
+
+#############################################################################################################
+#############################################################################################################
+
+def init():
+    global K
+
+    st.set_page_config(
+        page_title="BigBasket Products",
+        page_icon="🧺",
+        layout="centered",
+        initial_sidebar_state="expanded",
+    )
+
+    with st.sidebar:
+
+        st.markdown(
+            """This is an app interface for [Document-Retrieval](https://python.langchain.com/docs/modules/data_connection/) on :green[BigBasketProducts.csv] using :blue[Streamlit].
+            This Query Engine uses :blue[Qdrant] for vector DB & :blue[LangChain] for performing semantic search.  
+            [![VectorDB: Qdrant](https://img.shields.io/badge/VectorDB-Qdrant-blue)](https://github.com/qdrant/qdrant)
+            [![Embeddings: OpenAI](https://img.shields.io/badge/Embeddings-OpenAI-blue)](https://platform.openai.com/docs/guides/embeddings/what-are-embeddings)
+            [![Retriever: LanghChain](https://img.shields.io/badge/Retriever-LanghChain-blue)](https://github.com/langchain-ai/langchain)
+            [![UI: streamlit](https://img.shields.io/badge/UI-streamlit-blue)](https://streamlit.io/)
+            """
+        )
+
+        mention(
+            label="Document-Retrieval",
+            icon="github",  # GitHub is also featured!
+            url="https://github.com/AbhishekPardhi/Document-Retrieval",
+        )
+
+        # st.write('---')
+
+        st.subheader('Parameters')
+        K = st.slider('K', 1, 10, K, help='Sets max number of products  \nthat can be retrieved')
+        
+
+
+    st.header('BigBasket Products',divider=True)
 
 def display_data(res):
     srcs = [json.loads(row.page_content) for row in res['source_documents']]
@@ -99,81 +184,81 @@ def display_data(res):
     )
 
 def main():
-    global K
-    st.set_page_config(
-        page_title="BigBasket Products",
-        page_icon="🧺",
-        layout="centered",
-        initial_sidebar_state="expanded",
-    )
 
-    with st.sidebar:
-
-        st.markdown(
-            """This is an app interface for [Document-Retrieval](https://python.langchain.com/docs/modules/data_connection/) on :green[BigBasketProducts.csv] using :blue[Streamlit].
-            This Query Engine uses :blue[Qdrant] for vector DB & :blue[LangChain] for performing semantic search.  
-            [![VectorDB: Qdrant](https://img.shields.io/badge/VectorDB-Qdrant-blue)](https://github.com/qdrant/qdrant)
-            [![Embeddings: OpenAI](https://img.shields.io/badge/Embeddings-OpenAI-blue)](https://platform.openai.com/docs/guides/embeddings/what-are-embeddings)
-            [![Retriever: LanghChain](https://img.shields.io/badge/Retriever-LanghChain-blue)](https://github.com/langchain-ai/langchain)
-            [![UI: streamlit](https://img.shields.io/badge/UI-streamlit-blue)](https://streamlit.io/)
-            """
-        )
-
-        mention(
-            label="Document-Retrieval",
-            icon="github",  # GitHub is also featured!
-            url="https://github.com/AbhishekPardhi/Document-Retrieval",
-        )
-
-        # st.write('---')
-
-        st.subheader('Parameters')
-        K = st.slider('K', 1, 10, 4, help='Sets max number of products  \nthat can be retrieved')
-        
-
-
-    st.header('BigBasket Products',divider=True)
+    init()
 
     # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Hello 👋\n\n I am here to help you choose the product that you wanna buy!"}
+        ]
     
     # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+    # for message in st.session_state.messages:
+    #     with st.chat_message(message["role"]):
+    #         st.markdown(message["content"])
 
     retriever = Retriever()
 
-    with st.chat_message("assistant"):
-        st.write("Hello 👋\n\n I am here to help you choose the product that you wanna buy!")
+    # with st.chat_message("assistant"):
+    #     st.write("Hello 👋\n\n I am here to help you choose the product that you wanna buy!")
 
-    if prompt:=st.chat_input("Say something"):
+    if prompt:=st.chat_input("Say something"): # Prompt for user input and save to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
-        with st.chat_message("user"):
-            st.markdown(f'{prompt}')
+    for message in st.session_state.messages: # Display the prior chat messages
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
-        # Recommend me some product using which I can make bread
-
-        res = search(retriever, prompt)
-        answer = res['answer']
-        # answer = prompt
-
-        # Display assistant response in chat message container
+    # If last message is not from assistant, generate a new response
+    if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            full_response = ""
+            with st.spinner("Thinking..."):
+                res = search(retriever, prompt)
+                answer = res['answer']
+                # st.write(response.response)
+                message = {"role": "assistant", "content": answer}
+                st.session_state.messages.append(message) # Add response to message history
 
-            # Simulate stream of response with milliseconds delay
-            for chunk in answer.split():
-                full_response += chunk + " "
-                time.sleep(0.05)
-                # Add a blinking cursor to simulate typing
-                message_placeholder.markdown(full_response + "▌")
-            message_placeholder.markdown(full_response)
+                # Display assistant response in chat message container
+                message_placeholder = st.empty()
+                full_response = ""
 
-            # Dsiplay product details
-            display_data(res)
+                # Simulate stream of response with milliseconds delay
+                for chunk in answer.split():
+                    full_response += chunk + " "
+                    time.sleep(0.05)
+                    # Add a blinking cursor to simulate typing
+                    message_placeholder.markdown(full_response + "▌")
+                message_placeholder.markdown(full_response)
+
+                # Dsiplay product details
+                display_data(res)
+
+        # with st.chat_message("user"):
+        #     st.markdown(f'{prompt}')
+
+        # # Recommend me some product using which I can make bread
+
+        # res = search(retriever, prompt)
+        # answer = res['answer']
+        # # answer = prompt
+
+        # # Display assistant response in chat message container
+        # with st.chat_message("assistant"):
+        #     message_placeholder = st.empty()
+        #     full_response = ""
+
+        #     # Simulate stream of response with milliseconds delay
+        #     for chunk in answer.split():
+        #         full_response += chunk + " "
+        #         time.sleep(0.05)
+        #         # Add a blinking cursor to simulate typing
+        #         message_placeholder.markdown(full_response + "▌")
+        #     message_placeholder.markdown(full_response)
+
+        #     # Dsiplay product details
+        #     display_data(res)
 
 
 
