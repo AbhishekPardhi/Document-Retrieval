f681889b770ee1a528838ebea4912fb3dbc4aaf8
Abhishek Pardhi <apardhi20@iitk.ac.in>
2023-11-26 06:52:34 +0530

    Done

--- backend/init_collections_products.py
+++ backend/init_collections_products.py
@@ -2,11 +2,11 @@ import sys
 sys.path.append("..")
 import os.path
 from tqdm import tqdm
-
+import pandas
+import time
 import qdrant_client
 from langchain.vectorstores import Qdrant
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.document_loaders.csv_loader import CSVLoader
 
 from backend.config import DATA_DIR, QDRANT_URL, QDRANT_API_KEY, COLLECTION_NAME
 
@@ -27,12 +27,13 @@ def upload_embeddings():
     # Load data
     file_path = os.path.join(DATA_DIR, 'bigBasketProducts.csv')
 
-    loader = CSVLoader(file_path=file_path, source_column=('rating'), csv_args={'delimiter': ','}, autodetect_encoding=True)
-    documents = loader.load()
-    documents = documents[:100]
+    df = pandas.read_csv(file_path)
+    metadatas = [{'source':int(df.loc[i][0]), 'row':i} for i in range(len(df))]
+    df = df.apply(lambda x: x.to_json(), axis=1)
 
-    docs = [doc.page_content for doc in documents]
-    metadatas = [doc.metadata for doc in documents]
+    print(df[0])
+
+    docs = [row for row in df]
 
     client.recreate_collection(
         collection_name=COLLECTION_NAME,
@@ -48,12 +49,24 @@ def upload_embeddings():
         embeddings=embeddings,
     )
 
-    # add documents to vector store
-    vector_store.add_texts(
-        texts=docs,
-        metadatas=metadatas,
-        ids=[str(i) for i in tqdm(range(len(docs)))],
-    )
+    retries_dict = {}
+
+    for i in range(0, len(docs), 32):
+        try:
+            vector_store.add_texts(
+                texts=docs[i:i+32],
+                metadatas=metadatas[i:i+32],
+                ids=tqdm.tqdm(range(i, i+32))
+            )
+        except Exception as e:
+            print(i, e)
+            i = i - 32
+            retries_dict[i] = retries_dict.get(i, 0) + 1
+            if retries_dict[i] > 5:
+                print(f"Failed to add documents at index {i} after 3 retries. Skipping...")
+                i += 32
+                continue
+            time.sleep(1)
 
 
 if __name__ == '__main__':
